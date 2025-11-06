9dceea0a1704341c994d193e98d0e400e29229bb
Robalian <robalian94@gmail.com>
2025-11-06 22:58:12 +0100

    Initial commit

---
+++ Src/Player.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+enum ELookDirection {
+	Right,
+	Left
+}
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 200.0f;
+	
+	private AnimatedSprite2D Sprite;
+	
+	private ELookDirection LookDirection = ELookDirection.Right; 
+
+
+	public override void _Ready()
+	{
+		this.Sprite = GetNode<AnimatedSprite2D>("Sprite");
+	}
+
+	private void _UpdateAnimation(Vector2 direction) {
+		if (direction.X < 0)
+			this.LookDirection = ELookDirection.Left;
+		else if (direction.X > 0)
+			this.LookDirection = ELookDirection.Right;
+		
+		string suffix = this.LookDirection == ELookDirection.Left ? "_L" : "_R";
+		if (direction.Length() > 0) {
+			this.Sprite.Play($"walk{suffix}");
+		}
+		else {
+			this.Sprite.Play($"idle{suffix}");
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction.Normalized() * Speed;
+		}
+		else
+		{
+			velocity = Vector2.Zero;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+		
+		_UpdateAnimation(direction);
+	}
+}
